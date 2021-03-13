7c3acceea2c948580a6ff5613dea9bab8ccbb421
Sergio Rojas <48057303+hisergiorojas@users.noreply.github.com>
2021-03-13 22:11:31 +0000

    add otio convert

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, flash, request, redirect, render_template, send_file
 from werkzeug.utils import secure_filename
+import os
 import opentimelineio as otio
 
 app = Flask(__name__)
@@ -11,16 +12,14 @@ def upload_form():
 
 @app.route('/', methods = ['POST'])
 def upload_file():
-    if request.method == 'POST':
-        
+    if request.method == 'POST':        
         f = request.files['file']
         f.save(secure_filename(f.filename))
 
-        print(f.filename)
-        return 'h'
-#        timeline = otio.adapters.read_from_file()
- #       otio.adapters.write_to_file(timeline, )
+        filename = os.path.splitext(f.filename)[0]
+        timeline = otio.adapters.read_from_file(f.filename)
+        download_filename = filename + '.otio'
+       
+        otio.adapters.write_to_file(timeline, download_filename)
 
-
-  #      return send_file(path, as_attachment=True)
-        
+        return send_file(download_filename, as_attachment=True)
