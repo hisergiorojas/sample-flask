5bd79563348dced2971d65e71060b419d947f767
Sergio Rojas <48057303+hisergiorojas@users.noreply.github.com>
2021-04-29 05:04:25 +0000

    Add requests

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def upload_form():
-    response = requests.get('.https://api.pandascore.co/matches/upcoming?token=1nW7K4Ys0yeOe2Uqq4yTIUjr_szzLxuR1-dLhPDIgVxkfbCkHrQ')
+    response = requests.get('https://api.pandascore.co/matches/upcoming?token=1nW7K4Ys0yeOe2Uqq4yTIUjr_szzLxuR1-dLhPDIgVxkfbCkHrQ')
     print(response.json())
     return render_template("index.html")
 
