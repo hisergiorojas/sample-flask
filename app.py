f475f8f8b3d624995a71f23cabb5f4b10f4fcbb6
Sergio Rojas <48057303+hisergiorojas@users.noreply.github.com>
2021-05-18 02:26:14 +0000

    Add timeline

--- app.py
+++ app.py
@@ -46,6 +46,8 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            timeline = otio.adapters.read_from_file(filename)
+            print('timeline', timeline)
             flash('File successfully uploaded')
             return redirect('/')
         else:
