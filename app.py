cbbc927e523966c136eee3a766b2814586429e94
Sergio Rojas <48057303+hisergiorojas@users.noreply.github.com>
2021-05-18 02:04:47 +0000

    Update allowed extenions

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if not os.path.isdir(UPLOAD_FOLDER):
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['edl'])
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
