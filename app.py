3007543fb65b343ff720114974b99b6cafcf863f
Sergio Rojas <48057303+hisergiorojas@users.noreply.github.com>
2021-05-18 01:02:33 +0000

    Update

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.',1)[1].lower() in  ALLOWED_EXTENSIONS
-
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def upload_form(): 
