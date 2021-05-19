494e7654a4e104bcd787eaffb67903accb45e5e4
Sergio Rojas <48057303+hisergiorojas@users.noreply.github.com>
2021-05-19 18:06:00 +0000

    Update USD install

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 echo 'hello world'
 git clone https://github.com/PixarAnimationStudios/USD.git
-echo 'finished cloning'
-ls
+sudo apt-get update
+sudo apt-get install libglu1-mesa-dev freeglut3-dev mesa-common-dev
+pip3 install PySide2
+pip3 install PyOpenGL
+python USD/build_scripts/build_usd.py ../local/USD
